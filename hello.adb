741acbe9db858446063296c85e492b28010588ba
mark holland <mark@partiallogic.com>
2014-09-24 18:24:06 +0200

    Added loop

--- hello.adb
+++ hello.adb
@@ -1,7 +1,12 @@
 with Ada.Text_IO;
  use Ada.Text_IO;
+
+type My_Int1 is range 0..100;
+
  procedure Hello is
  begin
+ for I in My_Int1'range loop
  Put_Line ("Hello WORLD!");
+ end loop;
  end
  Hello;
