2d6c19c66d3b1095e316bc7111fb8aedd7d99abd
Mark Holland <mark@partiallogic.com>
2014-09-24 18:19:01 +0200

    Formatting of hello.adb

--- hello.adb
+++ hello.adb
@@ -1,1 +1,7 @@
-with Ada.Text_IO; use Ada.Text_IO; procedure Hello is begin Put_Line ("Hello WORLD!"); end Hello;
+with Ada.Text_IO;
+ use Ada.Text_IO;
+ procedure Hello is
+ begin
+ Put_Line ("Hello WORLD!");
+ end
+ Hello;
