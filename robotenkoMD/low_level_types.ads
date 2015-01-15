f7a8c0aa599850462456ec10875bd81f1695428a
mark holland <markholland@marks-MacBook-Air.local>
2015-01-15 16:46:16 +0100

    Removed old project

--- robotenkoMD/low_level_types.ads
+++
@@ -1,8 +0,0 @@
-with Interfaces;
-
-package Low_Level_Types is
-
-   type Word is new Interfaces.Unsigned_16;
-   type Byte is new Interfaces.Unsigned_8;
-
-end Low_Level_Types;
--- robotenkoMD/port_io_linux.ads
+++
@@ -1,9 +0,0 @@
-with Low_Level_Types; use Low_Level_Types;
-
-package Port_IO_Linux is
-
-   procedure Port_Out(Port : in Word; Data : in Byte);
-
-   function Port_In(Port : in Word) return Byte;
-
-end Port_IO_Linux;
